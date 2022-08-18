5d7ca98e8cfdb8fa2401419082f00ef9b666294d
Log <391777866@qq.com>
2022-08-18 11:43:27 +0800

    feat:add /delete api

--- tracker/app.py
+++ tracker/app.py
@@ -11,16 +11,20 @@ CORS(app, supports_credentials=True)
 @app.route('/register',methods=['GET','POST'])
 def register():  # put application's code here
     data = flask.request.get_data()
+    print("register:")
     print(data)
     return 'Hello World!'
 
 @app.route('/delete',methods=['GET','POST'])
 def delete():
-    pass
+    data = flask.request.get_data()
+    print("delete:")
+    print(data)
+    return "delete done."
 
 @app.route('/update',methods=['GET','POST'])
 def heartbeat():
     pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
