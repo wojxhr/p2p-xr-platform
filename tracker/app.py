5c17c63112a22a6468ff9cd43cf372c13e129a0a
unknown <391777866@qq.com>
2022-08-23 14:41:58 +0800

    feat:complete basic tracker.

--- tracker/app.py
+++ tracker/app.py
@@ -7,22 +7,36 @@ import json
 peerNum = 0
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+node=dict()
+node_test = []
+
 
 @app.route('/register',methods=['GET','POST'])
 def register():  # put application's code here
-    data = flask.request.get_data()
-    print("register:")
-    print(data)
-    return 'Hello World!'
+    global peerNum
+    data = flask.request.json
+    peerId = data['peerId']
+    node[peerId] = {"videoinfo":1,"segmentinfo":2}
+    peerNum+=1
+    print(node)
+    return 'register done!'
 
 @app.route('/delete',methods=['GET','POST'])
 def delete():
-    data = flask.request.get_data()
-    print("delete:")
-    print(data)
+    data = flask.request.json
+    peerId = data['peerId']
+    del node[peerId]
+    print(node)
     return "delete done."
 
-@app.route('/update',methods=['GET','POST'])
+@app.route('/applyid',methods=['GET'])
+def apply():
+    content = json.dumps(node)
+    print(content)
+    return content
+
+
+@app.route('/update',methods=['POST'])
 def heartbeat():
     pass
 
